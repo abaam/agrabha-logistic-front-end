032a13e0a99969b889cc2f905f9dd2c129659014
charles <charlesmarnielimpo@gmail.com>
2022-01-02 14:05:35 +0800

    Added input and button components

---
+++ src/components/buttons/ButtonOutlineGreen.vue
@@ -0,0 +1,20 @@
+<template>
+    <button :type="type" class="group relative w-full flex justify-center py-2 px-4 border border-green-light text-sm font-medium rounded-md bg-white text-green-light hover:bg-green hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-light">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: "submit"
+            },
+            buttonText: {
+                type: String,
+                default: ''
+            },
+        }
+    }
+</script>
---
+++ src/components/buttons/ButtonSolidBlue.vue
@@ -0,0 +1,20 @@
+<template>
+    <button :type="type" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-light hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-light">
+        {{ buttonText }}
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: "submit"
+            },
+            buttonText: {
+                type: String,
+                default: ''
+            },
+        }
+    }
+</script>

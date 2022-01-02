b2ab35c5dab13abbc6e294a8df6cb272a0b34898
charles <charlesmarnielimpo@gmail.com>
2022-01-02 14:07:34 +0800

    Responsive login and registration page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex items-center min-h-screen">
-        <div class="w-1/3">
+        <div class="w-0 lg:w-1/3">
             <img class="min-h-screen object-cover z-0" src="img/login-image.png" alt="">
         </div>
-        <div class="w-2/3 px-16">
+        <div class="w-full lg:w-2/3 px-8 md:px-12 lg:px-16">
             <img src="img/agrabah-logo.png" class="h-20 mb-8" alt="">
 
             <h1 class="text-4xl text-blue font-bold py-4 border-b uppercase">Agrabah Logistics Login</h1>
@@ -11,16 +11,22 @@
 
             <form action="">
                 <div class="mt-10 space-y-4">
-                    <div>
-                        <label for="phone-number" class="block text-sm font-medium">Phone number</label>
-                        <input id="phone-number" name="phone_number" type="text" autocomplete="number" required="" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue focus:border-blue focus:z-10 sm:text-sm" placeholder="Phone number" />
-                    </div>
+                    <Input
+                        v-model="event.phoneNumber"
+                        type="text"
+                        id="phone-number"
+                        name="phone_number"
+                        label="Phone number" 
+                    />
 
-                    <div>
-                        <label for="password" class="block text-sm font-medium">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue focus:border-blue focus:z-10 sm:text-sm" placeholder="Password" />
-                    </div>
-
+                    <Input 
+                        v-model="event.password"
+                        type="password"
+                        id="password"
+                        name="password"
+                        label="Password" 
+                    />
+                    
                     <div class="flex items-center justify-between">
                         <div class="flex items-center">
                             <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-blue focus:ring-blue border-gray-300 rounded" />
@@ -37,12 +43,7 @@
                     </div>
 
                     <div>
-                        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-light hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-light">
-                            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-                            <LockClosedIcon class="h-5 w-5 text-blue group-hover:text-blue-light" aria-hidden="true" />
-                            </span>
-                            Sign in
-                        </button>
+                        <ButtonSolidBlue buttonText="Sign In" />
                     </div>
 
                     <div class="text-sm">
@@ -57,10 +58,23 @@
 
 <script>
     import { LockClosedIcon } from '@heroicons/vue/solid'
+    import Input from '../components/Input'
+    import ButtonSolidBlue from '../components/buttons/ButtonSolidBlue'
+    import ButtonOutlineGreen from '../components/buttons/ButtonOutlineGreen'
 
     export default {
         components: {
             LockClosedIcon,
+            Input,
+            ButtonSolidBlue
         },
+        data() {
+            return {
+                event: {
+                    phoneNumber: '',
+                    password: ''
+                }
+            }
+        }
     }
 </script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,34 +1,41 @@
 <template>
     <div class="flex items-center min-h-screen">
-        <div class="w-1/3">
+        <div class="w-0 lg:w-1/3">
             <img class="min-h-screen object-cover z-0" src="img/login-image.png" alt="">
         </div>
-        <div class="w-2/3 px-16">
+        <div class="w-full lg:w-2/3 px-8 md:px-12 lg:px-16">
             <img src="img/agrabah-logo.png" class="h-20 mb-8" alt="">
 
             <h1 class="text-4xl text-blue font-bold py-4 border-b uppercase">Agrabah Logistics Registration</h1>
 
             <form action="">
                 <div class="mt-10 space-y-4">
-                    <div>
-                        <label for="phone-number" class="block text-sm font-medium">Phone number</label>
-                        <input id="phone-number" name="phone_number" type="text" autocomplete="number" required="" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue focus:border-blue focus:z-10 sm:text-sm" placeholder="Phone number" />
-                    </div>
+                    <Input 
+                        v-model="event.phoneNumber"
+                        type="text"
+                        id="phone-number"
+                        name="phone_number"
+                        label="Phone number" 
+                    />
+
+                    <Input 
+                        v-model="event.password"
+                        type="password"
+                        id="password"
+                        name="password"
+                        label="Password" 
+                    />
+
+                    <Input 
+                        v-model="event.retypePassword"
+                        type="password"
+                        id="retype-password"
+                        name="retype_password"
+                        label="Re-type Password" 
+                    />
 
                     <div>
-                        <label for="password" class="block text-sm font-medium">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue focus:border-blue focus:z-10 sm:text-sm" placeholder="Password" />
-                    </div>
-
-                    <div>
-                        <label for="password" class="block text-sm font-medium">Re-type Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-blue focus:border-blue focus:z-10 sm:text-sm" placeholder="Password" />
-                    </div>
-
-                    <div>
-                        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-light hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-light">
-                            Register
-                        </button>
+                        <ButtonSolidBlue buttonText="Register" />
                     </div>
 
                     <div class="text-sm">
@@ -43,10 +50,23 @@
 
 <script>
     import { LockClosedIcon } from '@heroicons/vue/solid'
+    import Input from '../components/Input'
+    import ButtonSolidBlue from '../components/buttons/ButtonSolidBlue'
 
     export default {
         components: {
             LockClosedIcon,
+            Input,
+            ButtonSolidBlue
         },
+        data() {
+            return {
+                event: {
+                    phoneNumber: '',
+                    password: '',
+                    retypePassword: ''
+                }
+            }
+        }
     }
 </script>

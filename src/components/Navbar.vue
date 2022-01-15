aa091bf14763745a70e26fb216331e02a19ae26f
charles <charlesmarnielimpo@gmail.com>
2022-01-15 10:42:42 +0800

    Modified button and navbar components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="">
-        <nav class="flex justify-between items-center space-x-12 py-2 px-6 md:px-12 lg:px-16 border-b">
-            <a href="https://agrabah.ph">
-                <img class="w-64" src="../../public/img/agrabah-logo.png" alt="Agrabah">
-            </a>
+    <nav class="flex justify-start items-center space-x-12 py-2 px-6 md:px-12 lg:px-16 border-b mb-8">
+        <router-link to="/">
+            <img class="w-64 mr-6" src="../../public/img/agrabah-logo.png" alt="Agrabah">
+        </router-link>
             
-            <router-link to="/login" class="flex justify-center py-2 px-4 border border-blue-light text-sm font-medium rounded-md bg-white text-blue-light hover:bg-blue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-light">Login</router-link>
-        </nav>
-    </div>
+        <div class="flex-1 items-center justify-evenly space-x-8">
+            <router-link to="'/tracking" class="uppercase block tracking-wide font-semibold hover:text-blue">Tracking</router-link>
+        </div>
+
+        <router-link to="/login" class="flex justify-center py-2 px-4 border border-blue-light text-sm font-medium rounded-md bg-white text-blue-light hover:bg-blue hover:text-white focus:outline-none">Login</router-link>
+    </nav>
 </template>
 
 <script>
--- src/components/buttons/ButtonSolidBlue.vue
+++ src/components/buttons/ButtonSolidBlue.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :type="type" :class="buttonClass" class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-light hover:bg-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-light">
+    <button :type="type" :class="buttonClass" class="flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-light hover:bg-blue focus:outline-none focus:bg-blue">
         {{ buttonText }}
     </button>
 </template>

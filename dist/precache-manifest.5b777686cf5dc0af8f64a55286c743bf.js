5bd10ff6eafed86e2d553ec807610a9d9fb1933f
jordan.lopez <imjordanlopez@gmail.com>
2022-04-07 10:58:03 +0800

    update booking

---
+++ dist/precache-manifest.5b777686cf5dc0af8f64a55286c743bf.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a36efc6f1dc32a4d33",
+    "url": "/css/app.cccc0999.css"
+  },
+  {
+    "revision": "beba5f1d80e87066176255580c2725c0",
+    "url": "/img/agrabah-login-logo.png"
+  },
+  {
+    "revision": "06bf54194ba6e5f075e34d4fee978d9e",
+    "url": "/img/agrabah-logistics-logo.06bf5419.png"
+  },
+  {
+    "revision": "06bf54194ba6e5f075e34d4fee978d9e",
+    "url": "/img/agrabah-logistics-logo.png"
+  },
+  {
+    "revision": "c11cf161a558f9d78328eaf81f89bf05",
+    "url": "/img/agrabah-logo-white-icon.png"
+  },
+  {
+    "revision": "548a240ef935e49b31e096301b88d1a9",
+    "url": "/img/agrabah-logo-white.png"
+  },
+  {
+    "revision": "82bc75743cfc25ff460e5465ece1275d",
+    "url": "/img/agrabah-logo.png"
+  },
+  {
+    "revision": "07ce88fb347a7c1246966db7d33e8381",
+    "url": "/img/destination.07ce88fb.svg"
+  },
+  {
+    "revision": "2012e640504a7e360198c2c3c124d37a",
+    "url": "/img/gcash-logo.2012e640.png"
+  },
+  {
+    "revision": "2012e640504a7e360198c2c3c124d37a",
+    "url": "/img/gcash-logo.png"
+  },
+  {
+    "revision": "f6806a71906d2135ae3971509f18a08d",
+    "url": "/img/login-image.png"
+  },
+  {
+    "revision": "95a65a938904925009b9ae9d111f97ea",
+    "url": "/img/no_data.95a65a93.svg"
+  },
+  {
+    "revision": "6d5c2d05bf40611545764036bd3f031f",
+    "url": "/img/no_delivery.6d5c2d05.svg"
+  },
+  {
+    "revision": "3f481c5a284121a2c9b0fcdcae3a01e7",
+    "url": "/img/verification.3f481c5a.svg"
+  },
+  {
+    "revision": "49eb5a1cb2f0453b64f5f310837f92f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02a36efc6f1dc32a4d33",
+    "url": "/js/app.45bb21cb.js"
+  },
+  {
+    "revision": "6be4d25d9820999b74dd",
+    "url": "/js/chunk-2d0a3537.37043845.js"
+  },
+  {
+    "revision": "19f828041ab0551d7d3e",
+    "url": "/js/chunk-2d0c73e8.6bccbfad.js"
+  },
+  {
+    "revision": "903de537ed57ba93b4be",
+    "url": "/js/chunk-2d0c7b21.cd12a55a.js"
+  },
+  {
+    "revision": "4c0225310d39da3c6bcc",
+    "url": "/js/chunk-2d2086b7.0e55ba38.js"
+  },
+  {
+    "revision": "c8f69f5ad7bc9563bd8a",
+    "url": "/js/chunk-3ab67374.1bf221ae.js"
+  },
+  {
+    "revision": "f0c4e8a492143bebb527",
+    "url": "/js/chunk-4cbd9b70.b5edadef.js"
+  },
+  {
+    "revision": "6f3703a9d5701f556d75",
+    "url": "/js/chunk-4d2da700.6ec196ea.js"
+  },
+  {
+    "revision": "4fe727a19ca60eb9c6be",
+    "url": "/js/chunk-5598fa7e.2b5b8f2c.js"
+  },
+  {
+    "revision": "62c0117b6da79e44b2e0",
+    "url": "/js/chunk-562b61a7.8c0a70b1.js"
+  },
+  {
+    "revision": "35724ad5c36ee2da8a9b",
+    "url": "/js/chunk-64b904bc.fffadb92.js"
+  },
+  {
+    "revision": "f7d160b33b4ea37f4e5e",
+    "url": "/js/chunk-97a5e6c2.b41a24de.js"
+  },
+  {
+    "revision": "c79286533ea2edb9db1f",
+    "url": "/js/chunk-b4b2f6ee.74235d5f.js"
+  },
+  {
+    "revision": "e9f265d49956d5e7591c",
+    "url": "/js/chunk-b6ddfc5e.9eb94936.js"
+  },
+  {
+    "revision": "0323c7f5bf7ca7d98551",
+    "url": "/js/chunk-d1a5c766.04555385.js"
+  },
+  {
+    "revision": "11d5e6fd95376c71f170",
+    "url": "/js/chunk-vendors.62ecfcee.js"
+  },
+  {
+    "revision": "9c914436fc1919bbe044",
+    "url": "/js/register.4022c3e9.js"
+  },
+  {
+    "revision": "9d205a46202f0c9a0ec8cf102baf492a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "07ce88fb347a7c1246966db7d33e8381",
+    "url": "/svg/destination.svg"
+  },
+  {
+    "revision": "95a65a938904925009b9ae9d111f97ea",
+    "url": "/svg/no_data.svg"
+  },
+  {
+    "revision": "6d5c2d05bf40611545764036bd3f031f",
+    "url": "/svg/no_delivery.svg"
+  },
+  {
+    "revision": "3f481c5a284121a2c9b0fcdcae3a01e7",
+    "url": "/svg/verification.svg"
+  }
+]);
---
+++ src/views/customer/Transactions.vue
@@ -0,0 +1,480 @@
+<template>
+  <DashboardLayout mobileTitle="Transactions">
+    <section class="hidden px-3 py-6 md:block md:p-6">
+      <h2
+        class="mb-6 block text-xl font-bold leading-6 md:text-2xl lg:text-3xl"
+      >
+        Transactions
+      </h2>
+
+      <!-- Table -->
+      <div class="hidden gap-y-3 rounded-md bg-white py-6 shadow md:grid">
+        <div
+          class="flex items-center justify-between px-6 md:flex-col md:space-y-2 lg:flex-row lg:space-y-0"
+        >
+          <!-- Filter -->
+          <div class="flex items-center space-x-2">
+            <p>Show</p>
+            <select
+              v-model="show_entries"
+              @change="showEntries($event)"
+              class="focus:outline-none relative block w-20 appearance-none rounded border border-grey px-3 py-2 text-gray-900 focus:z-10 focus:border-grey-dark focus:ring-0 focus:ring-grey-dark"
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="25">25</option>
+              <option value="100">100</option>
+            </select>
+            <p>entries</p>
+          </div>
+
+          <!-- Search -->
+          <div class="flex items-center space-x-1">
+            <p>Search:</p>
+            <input
+              type="text"
+              @keyup="searchDelivery"
+              @keyup.enter="fetchDeliveries"
+              v-model="search"
+              class="focus:outline-none relative block w-full appearance-none rounded border border-grey px-3 py-2 text-gray-900 focus:z-10 focus:border-grey-dark focus:ring-0 focus:ring-grey-dark"
+            />
+          </div>
+        </div>
+
+        <section class="container mx-auto grid grid-cols-1">
+          <div class="-my-2 overflow-x-auto">
+            <div class="inline-block min-w-full py-2 align-middle">
+              <div class="overflow-hidden">
+                <table class="min-w-full divide-y divide-grey-light">
+                  <thead class="bg-grey-light bg-opacity-30">
+                    <tr>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider"
+                      >
+                        Delivery Date
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider"
+                      >
+                        Delivery ID
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider"
+                      >
+                        Origin
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider"
+                      >
+                        Destination
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider"
+                      >
+                        Cost
+                      </th>
+                      <th
+                        scope="col"
+                        class="whitespace-nowrap px-6 py-3 text-left text-sm font-semibold uppercase tracking-wider"
+                      >
+                        Status
+                      </th>
+                      <th scope="col" class="relative px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody
+                    v-if="deliveries != ''"
+                    class="divide-y divide-grey-light bg-white"
+                  >
+                    <tr v-for="delivery in deliveries" :key="delivery.id">
+                      <td class="whitespace-nowrap px-6 py-4">
+                        <div class="text-sm">{{ delivery.delivery_date }}</div>
+                      </td>
+                      <td class="whitespace-nowrap px-6 py-4">
+                        <div class="text-sm">{{ delivery.delivery_id }}</div>
+                      </td>
+                      <td class="whitespace-nowrap px-6 py-4">
+                        <div class="text-sm">{{ delivery.origin }}</div>
+                      </td>
+                      <td class="whitespace-nowrap px-6 py-4">
+                        <div class="text-sm">{{ delivery.destination }}</div>
+                      </td>
+                      <td class="whitespace-nowrap px-6 py-4">
+                        <div class="text-sm">{{ delivery.cost }}</div>
+                      </td>
+                      <td class="whitespace-nowrap px-6 py-4">
+                        <div v-if="delivery.status == 1">
+                          <div
+                            class="flex w-auto items-center justify-center rounded-full bg-green-light py-0.5 px-1"
+                          >
+                            <p class="text-sm font-semibold text-green">
+                              Delivered
+                            </p>
+                          </div>
+                        </div>
+                        <div v-else-if="delivery.status == 2">
+                          <div
+                            class="flex w-auto items-center justify-center rounded-full bg-blue-light py-0.5 px-1"
+                          >
+                            <p class="text-sm font-semibold text-blue">
+                              In Transit
+                            </p>
+                          </div>
+                        </div>
+                        <div v-else-if="delivery.status == 3">
+                          <div
+                            class="flex w-auto items-center justify-center rounded-full bg-orange-light py-0.5 px-1"
+                          >
+                            <p class="text-sm font-semibold text-orange">
+                              To Deliver
+                            </p>
+                          </div>
+                        </div>
+                      </td>
+                      <td
+                        class="whitespace-nowrap px-6 py-4 text-right text-sm"
+                      >
+                        <a href="#" class="text-blue-light hover:text-blue"
+                          >View details</a
+                        >
+                      </td>
+                    </tr>
+                  </tbody>
+                  <tbody v-else>
+                    <tr>
+                      <td colspan="7" class="p-6">
+                        <img
+                          class="mx-auto w-24"
+                          src="../../../public/svg/no_data.svg"
+                          alt="No records found"
+                        />
+                        <p class="mt-4 block text-center">No record found.</p>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div
+          class="flex items-center justify-between px-6 md:flex-col md:space-y-2 lg:flex-row lg:space-y-0"
+          v-if="deliveries != ''"
+        >
+          <p>
+            Showing <span>1</span> to <span>{{ show_entries }}</span> of
+            <span>{{ pagination.total }}</span> entries
+          </p>
+          <div>
+            <pagination :pagination="pagination" @paginate="fetchDeliveries" />
+          </div>
+        </div>
+      </div>
+      <!-- /Table -->
+    </section>
+
+    <section class="grid px-3 md:hidden md:p-6 md:py-6">
+      <TabGroup>
+        <TabList
+          class="sticky top-14 z-40 -mx-3 flex justify-evenly border-b border-grey-light bg-white md:mx-0"
+        >
+          <Tab v-slot="{ selected }" as="template">
+            <button
+              :class="[selected ? 'border-blue text-blue' : 'bg-white']"
+              class="w-full cursor-pointer border-b-2 border-transparent py-2 text-center text-sm font-semibold md:w-auto md:text-base"
+            >
+              To Deliver
+            </button>
+          </Tab>
+          <Tab v-slot="{ selected }" as="template">
+            <button
+              :class="[selected ? 'border-blue text-blue' : 'bg-white']"
+              class="w-full cursor-pointer border-b-2 border-transparent py-2 text-center text-sm font-semibold md:w-auto md:text-base"
+            >
+              In Transit
+            </button>
+          </Tab>
+          <Tab v-slot="{ selected }" as="template">
+            <button
+              :class="[selected ? 'border-blue text-blue' : 'bg-white']"
+              class="w-full cursor-pointer border-b-2 border-transparent py-2 text-center text-sm font-semibold md:w-auto md:text-base"
+            >
+              Delivered
+            </button>
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <div v-if="to_deliver != ''" class="my-3 grid gap-y-2">
+              <div
+                v-for="delivery in to_deliver"
+                :key="delivery.id"
+                class="grid gap-y-3 rounded-md bg-white p-3 shadow"
+              >
+                <div class="flex items-center justify-between">
+                  <p class="font-semibold">{{ delivery.delivery_id }}</p>
+                  <p class="font-bold">{{ delivery.cost }}</p>
+                </div>
+
+                <div class="block">
+                  <p>{{ delivery.description }}</p>
+                  <p class="text-sm text-grey">
+                    Weight: <span>{{ delivery.weight }}</span>
+                  </p>
+                </div>
+
+                <div class="flex items-center space-x-1 text-orange">
+                  <div
+                    class="flex h-5 w-5 items-center justify-center rounded-full bg-orange-light"
+                  >
+                    <CubeIcon class="h-3 w-3 text-orange" />
+                  </div>
+                  <p class="text-sm font-semibold text-orange">To Deliver</p>
+                </div>
+              </div>
+            </div>
+            <div v-else class="my-20 grid h-96 place-items-center gap-1 px-3">
+              <div class="px-3 py-6 text-center">
+                <img
+                  class="mx-auto w-48"
+                  src="../../../public/svg/no_delivery.svg"
+                  alt=""
+                />
+                <h6 class="mt-8 block text-xl font-semibold">
+                  No deliveries yet?
+                </h6>
+                <p class="block leading-6 text-grey-dark">
+                  You'll see all your deliveries to be delivered here.
+                </p>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div v-if="in_transit != ''" class="my-3 grid gap-y-2">
+              <div
+                v-for="delivery in in_transit"
+                :key="delivery.id"
+                class="grid gap-y-3 rounded-md bg-white p-3 shadow"
+              >
+                <div class="flex items-center justify-between">
+                  <p class="font-semibold">{{ delivery.delivery_id }}</p>
+                  <p class="font-bold">{{ delivery.cost }}</p>
+                </div>
+
+                <div class="block">
+                  <p>{{ delivery.description }}</p>
+                  <p class="text-sm text-grey">
+                    Weight: <span>{{ delivery.weight }}</span>
+                  </p>
+                </div>
+
+                <div class="flex items-center space-x-1 text-blue">
+                  <div
+                    class="flex h-5 w-5 items-center justify-center rounded-full bg-blue-light"
+                  >
+                    <TruckIcon class="h-3 w-3 text-blue" />
+                  </div>
+                  <p class="text-sm font-semibold text-blue">In Transit</p>
+                </div>
+              </div>
+            </div>
+            <div v-else class="my-20 grid h-96 place-items-center gap-1 px-3">
+              <div class="px-3 py-6 text-center">
+                <img
+                  class="mx-auto w-48"
+                  src="../../../public/svg/no_delivery.svg"
+                  alt=""
+                />
+                <h6 class="mt-8 block text-xl font-semibold">
+                  No deliveries yet?
+                </h6>
+                <p class="block leading-6 text-grey-dark">
+                  You'll see all your deliveries in transit here.
+                </p>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div v-if="delivered != ''" class="my-3 grid gap-y-2">
+              <div
+                v-for="delivery in delivered"
+                :key="delivery.id"
+                class="grid gap-y-3 rounded-md bg-white p-3 shadow"
+              >
+                <div class="flex items-center justify-between">
+                  <p class="font-semibold">{{ delivery.delivery_id }}</p>
+                  <p class="font-bold">{{ delivery.cost }}</p>
+                </div>
+
+                <div class="block">
+                  <p>{{ delivery.description }}</p>
+                  <p class="text-sm text-grey">
+                    Weight: <span>{{ delivery.weight }}</span>
+                  </p>
+                </div>
+
+                <div class="flex items-center space-x-1 text-green">
+                  <div
+                    class="flex h-5 w-5 items-center justify-center rounded-full bg-green-light"
+                  >
+                    <CheckIcon class="h-3 w-3 text-green" />
+                  </div>
+                  <p class="text-sm font-semibold text-green">Delivered</p>
+                </div>
+              </div>
+            </div>
+            <div v-else class="my-20 grid h-96 place-items-center gap-1 px-3">
+              <div class="px-3 py-6 text-center">
+                <img
+                  class="mx-auto w-48"
+                  src="../../../public/svg/no_delivery.svg"
+                  alt=""
+                />
+                <h6 class="mt-8 block text-xl font-semibold">
+                  No deliveries yet?
+                </h6>
+                <p class="block leading-6 text-grey-dark">
+                  You'll see all your delivered deliveries here.
+                </p>
+              </div>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </section>
+  </DashboardLayout>
+</template>
+
+<script>
+import axios from "axios";
+import _ from "lodash";
+import DashboardLayout from "@/views/DashboardLayout.vue";
+import Pagination from "@/components/Pagination";
+import { TruckIcon, CubeIcon, CheckIcon } from "@heroicons/vue/outline";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
+
+export default {
+  data() {
+    return {
+      offset: 4,
+      pagination: {},
+      deliveries: [],
+      to_deliver: [],
+      in_transit: [],
+      delivered: [],
+      search: "",
+      show_entries: "5",
+    };
+  },
+  created() {
+    this.fetchDeliveries(),
+      this.fetchToDeliver(),
+      this.fetchInTransit(),
+      this.fetchDelivered();
+  },
+  methods: {
+    fetchDeliveries() {
+      let current_page = this.pagination.current_page;
+      let pageNum = current_page ? current_page : 1;
+
+      if (this.search == "") {
+        axios
+          .get(
+            process.env.VUE_APP_API +
+              `deliveries?page=${pageNum}&entries=${this.show_entries}`
+          )
+          .then((response) => {
+            this.pagination = response.data.deliveries.pagination;
+            this.deliveries = response.data.deliveries.collection;
+          });
+      } else {
+        axios
+          .get(
+            process.env.VUE_APP_API +
+              `deliveries/search?q=${this.search}&page=${pageNum}&entries=${this.show_entries}`
+          )
+          .then((response) => {
+            this.pagination = response.data.deliveries.pagination;
+            this.deliveries = response.data.deliveries.collection;
+          });
+      }
+    },
+
+    searchDelivery: _.debounce(function () {
+      if (this.search != "") {
+        axios
+          .get(
+            process.env.VUE_APP_API +
+              `deliveries/search?q=${this.search}&entries=${this.show_entries}`
+          )
+          .then((response) => {
+            this.pagination = response.data.deliveries.pagination;
+            this.deliveries = response.data.deliveries.collection;
+          });
+      }
+    }),
+
+    showEntries(event) {
+      if (this.search == "") {
+        axios
+          .get(
+            process.env.VUE_APP_API + "deliveries?entries=" + event.target.value
+          )
+          .then((response) => {
+            this.pagination = response.data.deliveries.pagination;
+            this.deliveries = response.data.deliveries.collection;
+            this.show_entries = event.target.value;
+          });
+      } else {
+        axios
+          .get(
+            process.env.VUE_APP_API +
+              `deliveries/search?q=${this.search}&entries=${this.show_entries}`
+          )
+          .then((response) => {
+            this.pagination = response.data.deliveries.pagination;
+            this.deliveries = response.data.deliveries.collection;
+          });
+      }
+    },
+
+    fetchToDeliver() {
+      axios.get(process.env.VUE_APP_API + `deliveries`).then((response) => {
+        this.to_deliver = response.data.to_deliver;
+      });
+    },
+
+    fetchInTransit() {
+      axios.get(process.env.VUE_APP_API + `deliveries`).then((response) => {
+        this.in_transit = response.data.in_transit;
+      });
+    },
+
+    fetchDelivered() {
+      axios.get(process.env.VUE_APP_API + `deliveries`).then((response) => {
+        this.delivered = response.data.delivered;
+      });
+    },
+  },
+  components: {
+    TabGroup,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel,
+    pagination: Pagination,
+    TruckIcon,
+    CubeIcon,
+    CheckIcon,
+    DashboardLayout,
+  },
+};
+</script>

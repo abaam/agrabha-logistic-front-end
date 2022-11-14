51c61de573156fc66761285df22934b82afab6c0
Xavier Emmanuel Rebotica <seybyer.emanwel@gmail.com>
2022-11-14 10:49:12 +0800

    Show tracking updates

--- src/views/TrackingDetails.vue
+++ src/views/TrackingDetails.vue
@@ -23,24 +23,13 @@
 
             
             <ol class="relative border-l border-gray-200 dark:border-gray-700">                  
-                <li class="mb-10 ml-4">
+                <li class="mb-10 ml-4" v-for="(tracking_detail, index) in tracking_details" :key="tracking_detail.id">
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
-                    <h3 class="text-md font-semibold text-gray-900 dark:text-white">Item departed at Daraga, Albay. 4500</h3>
+                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{ tracking_detail.date_time }}</time>
+                    <h3 v-if="index === 1" class="text-md font-semibold text-gray-900 dark:text-white">{{ tracking_detail.tracking_status }}</h3>
+                    <h3 v-else class="text-md font-semibold text-gray-900 dark:text-white">{{ tracking_detail.tracking_status }} {{ tracking_detail.location }}</h3>
                     <!-- <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing pages.</p> -->
                     <!-- <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="ml-2 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a> -->
-                </li>
-                <li class="mb-10 ml-4">
-                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2022</time>
-                    <h3 class="text-md font-semibold text-gray-900 dark:text-white">Item arrived at Daraga, Albay</h3>
-                    <!-- <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p> -->
-                </li>
-                <li class="ml-4">
-                    <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022</time>
-                    <h3 class="text-md font-semibold text-gray-900 dark:text-white">Item has been picked up by our driver</h3>
-                    <!-- <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p> -->
                 </li>
             </ol>
 
@@ -78,10 +67,34 @@
 <script>
     import Navigation from '../components/Navigation.vue'
     import Input from '../components/Input.vue'
+    import axios from "axios";
     import ButtonSolidGreen from '../components/buttons/ButtonSolidGreen.vue'
 
     export default {
         name: 'Tracking',
-        components: { Navigation, Input, ButtonSolidGreen }
+        components: { Navigation, Input, ButtonSolidGreen },
+        data() {
+            return {
+                tracking_details:[]
+            }
+        },
+        mounted() {
+            let tracking_id = window.location.pathname.split('/').pop();
+            console.log(tracking_id);
+
+            axios.get(process.env.VUE_APP_API + `tracking/details/${this.$route.params.id}`, {
+                withCredentials: true,  
+                headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('csrf_token'),
+                "Access-Control-Allow-Origin": "*"
+                }
+            }).then(response=>{
+                this.tracking_details = response.data.tracking;
+                console.log(this.tracking_details);
+            }).catch(error=>{
+                console.log(error)
+            })
+        }
     }
 </script>
